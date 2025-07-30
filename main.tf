1872060f09231474c523c93bd61f182d086c1988
katehjd21 <katedrew2103@hotmail.co.uk>
2025-07-30 16:59:31 +0100

    configured docker

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "app_server" {
   subnet_id              = "subnet-84e2f4fc"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name               = aws_key_pair.generated_key.key_name
-  user_data              = <<EOF
+  user_data              = <<-EOF
 #!/bin/bash
 mkdir -p /home/ubuntu/.ssh
 echo "${tls_private_key.ssh_key.public_key_openssh}" > /home/ubuntu/.ssh/authorized_keys
@@ -87,7 +87,7 @@ chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 chmod 600 /home/ubuntu/.ssh/authorized_keys
 
 sudo apt update -y
-sudo apt install -y nginx
+sudo apt install -y nginx docker.io git
 
 PUBLIC_IP=$(curl ifconfig.me)
 
@@ -108,9 +108,32 @@ EOL
 
 sudo ln -sf /etc/nginx/sites-available/my-site.conf /etc/nginx/sites-enabled/my-site.conf
 sudo rm -f /etc/nginx/sites-enabled/default
-
 sudo systemctl restart nginx
+
+# Enable and start Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Clone your app repo
+cd ~
+git clone https://github.com/ivo-velev123/turbogears.git
+cd turbogears
+
+# Build and run your Docker container
+sudo docker build -t myapp .
+sudo docker run -d -p 8080:8080 --name myapp myapp
+
+
 EOF
+
+
+
+
+
+
+
+
+
 
 
   tags = {
