a22897f6a85975ff994e614ea3964367211194d3
katehjd21 <katedrew2103@hotmail.co.uk>
2025-08-03 17:06:57 +0100

    refactored bash script to cloud-init

--- main.tf
+++ main.tf
@@ -79,65 +79,18 @@ resource "aws_instance" "app_server" {
   subnet_id              = "subnet-84e2f4fc"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name               = aws_key_pair.generated_key.key_name
-  user_data              = <<-EOF
-#!/bin/bash
-mkdir -p /home/ubuntu/.ssh
-echo "${tls_private_key.ssh_key.public_key_openssh}" > /home/ubuntu/.ssh/authorized_keys
-chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-chmod 600 /home/ubuntu/.ssh/authorized_keys
 
-sudo apt update -y
-sudo apt install -y nginx docker.io git
-
-PUBLIC_IP=$(curl ifconfig.me)
-
-cat << EOL | sudo tee /etc/nginx/sites-available/my-site.conf
-server {
-    listen 80;
-    server_name $PUBLIC_IP;
-
-    location / {
-        proxy_pass http://127.0.0.1:8080;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-}
-EOL
-
-sudo ln -sf /etc/nginx/sites-available/my-site.conf /etc/nginx/sites-enabled/my-site.conf
-sudo rm -f /etc/nginx/sites-enabled/default
-sudo systemctl restart nginx
-
-# Enable and start Docker
-sudo systemctl enable docker
-sudo systemctl start docker
-
-# Clone your app repo
-cd ~
-git clone https://github.com/ivo-velev123/turbogears.git
-cd turbogears
-
-# Build and run your Docker container
-sudo docker build -t myapp .
-sudo docker run -d -p 8080:8080 --name myapp myapp
-
-
-EOF
-
-
-
-
-
-
-
-
-
+  user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
+    instance_name = "terraform-practice"
+  })
 
 
   tags = {
     Name = "terraform-practice"
   }
 }
+
+
+
+
 
