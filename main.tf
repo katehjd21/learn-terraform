cfdea34940b2de43f91c95d55bdc0d1bdd747df7
katehjd21 <katedrew2103@hotmail.co.uk>
2025-08-04 12:05:38 +0100

    testing

--- main.tf
+++ main.tf
@@ -26,12 +26,53 @@ resource "local_file" "public_key" {
   filename = "./.ssh/terraform_rsa.pub"
 }
 
+resource "aws_security_group" "app_sg" {
+  name        = "app-security-group"
+  description = "Allow SSH, HTTP, and 8080"
+  vpc_id      = "vpc-367e885f"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-security-group"
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = "subnet-84e2f4fc"
-
-
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
   user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
     instance_name = "terraform-practice"
   })
